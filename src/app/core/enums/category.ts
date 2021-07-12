e812582a35cc6f1a80854c443b85d1c458d51356
Anton Nichitenco <ANichitenco@ellipsesolutions.com>
2021-07-12 19:02:55 +0300

    Added images, some changes in footer and header. Added list of categories to home page

---
+++ src/app/core/enums/category.ts
@@ -0,0 +1,10 @@
+export const enum Category {
+    Other               = 'Other',
+    Electronics         = 'Electronics',
+    MusicalInstruments  = 'Musical Instruments',
+    Clothes             = 'Clothes',
+    Fishing             = 'Fishing',
+    AutoAccessories     = 'AutoAccessories',
+    HomeAndGarden       = 'Home & Garden',
+    Sports              = 'Sports'
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "src/app/shared";
 import { OrdersComponent } from "./orders/orders.component";
 import { HomeComponent } from './home/home.component';
-
+import { MaterialModule } from "../shared/material/material.module";
 
 @NgModule({
     imports: [
-        SharedModule
+        SharedModule,
+        MaterialModule
     ],
     declarations: [
         OrdersComponent,
-        HomeComponent
+        HomeComponent,
     ]
 })
 
